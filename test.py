9314f9a079f151c8b281fd9f5d50892e8ab0b5f5
Govind S B <b.s.dnivog@gmail.com>
2023-12-03 08:53:12 +0530

    cleaned up test script to optimized ver

--- test.py
+++ test.py
@@ -1,31 +1,14 @@
 import time
-
-import tomesd
 start = time.time()
 
 # MODEL LOADING
 
-from diffusers import AutoPipelineForText2Image,AutoencoderKL,AutoencoderTiny
+from diffusers import AutoPipelineForText2Image,AutoencoderTiny
 import torch
 
 pipe = AutoPipelineForText2Image.from_pretrained("stabilityai/sdxl-turbo" , torch_dtype=torch.float16, variant="fp16")
-
-# pipe.upcast_vae()
-# pipe.vae = AutoencoderKL.from_pretrained("madebyollin/sdxl-vae-fp16-fix", torch_dtype=torch.float16)
 pipe.vae = AutoencoderTiny.from_pretrained("madebyollin/taesdxl", torch_dtype=torch.float16)
-
-# pipe.unet = torch.compile(pipe.unet, mode="reduce-overhead", fullgraph=True)
-# pipe.unet.to(memory_format=torch.channels_last)
-
-# pipe.enable_xformers_memory_efficient_attention()
-
-
 pipe.to("cuda")
-# pipe.enable_vae_slicing()
-# pipe.enable_model_cpu_offload()
-# pipe.enable_sequential_cpu_offload()
-
-# tomesd.apply_patch(pipe, ratio=0.5)
 
 load = time.time()
 
@@ -42,15 +25,10 @@ initial_generation = time.time()
 prompts = [ "A cinematic shot of a baby racoon wearing an intricate italian priest robe." ,
             "A person celebrating with a finished puzzle" ,
             "Astronaut in a jungle, cold color palette, muted colors, detailed, 8k",
-            "A majestic lion jumping from a big stone at night" ]
-
-'''
-# Batch Inference via pipeline
-image_arr = pipe(prompt=prompts, num_inference_steps=1, guidance_scale=0.0).images
-
-for i in range(len(image_arr)):
-    image_arr[i].save(f"test_{i}.jpg")
-'''
+            "A majestic lion jumping from a big stone at night" , 
+            "style of Shawn Coss, iridescent, by Takato Yamamoto, (pointed, Harlem Renaissance Art but extremely beautiful), (intricate details, masterpiece, best quality) , in the style of nicola samori, High Fashion, dynamic, dramatic, haute couture, elegant, ornate clothing, High Fashion, looking at viewer",
+            "cinematic photo of a jucy hamburger in a plate, 1970's kitchen, 35mm photograph, film, bokeh, professional, 4k, highly detailed",
+            "ethical dilemma"]
 
 for i in range(len(prompts)):
     image = pipe(prompt=prompts[i], num_inference_steps=1, guidance_scale=0.0).images[0]
@@ -62,7 +40,6 @@ total_time = final_generation - start
 calculated_load_time = load - start
 calculated_init_gen_time = initial_generation - load
 calculated_generation_per_img = (final_generation - initial_generation) / len(prompts)
-
 
 print(f"Total Time : {total_time}")
 print(f"Load Time : {calculated_load_time}")
